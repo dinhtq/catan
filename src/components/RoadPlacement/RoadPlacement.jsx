07975ffe597f70d0e49d2b84af4fae8639528601
Jason Dinh <dinhtq1@gmail.com>
2024-10-10 18:02:31 -0700

    created RoadPlacement

---
+++ src/components/RoadPlacement/RoadPlacement.jsx
@@ -0,0 +1,148 @@
+import { Box } from '@mui/material'
+
+export const roadsPlacements = [
+  {
+    title: 'top-left-road',
+    style: {
+      position: 'absolute',
+      top: '-24px',
+      left: '-2px',
+      width: '10px',
+      height: '50px',
+      backgroundColor: 'white',
+      transform: 'rotate(60deg)',
+    },
+  },
+  {
+    title: 'top-right-road',
+    style: {
+      position: 'absolute',
+      top: '-23px',
+      right: '62px',
+      width: '10px',
+      height: '50px',
+      backgroundColor: 'purple',
+      transform: 'rotate(-60deg)',
+    },
+  },
+  {
+    title: 'left-road',
+    style: {
+      position: 'absolute',
+      top: '39px',
+      left: '-38px',
+      width: '10px',
+      height: '50px',
+      backgroundColor: 'blue',
+      transform: 'rotate(0deg)',
+    },
+  },
+]
+
+export const lastHexInRowStyle = {
+  position: 'absolute',
+  top: '39px',
+  right: '25px',
+  width: '10px',
+  height: '50px',
+  backgroundColor: 'burlywood',
+  transform: 'rotate(0deg)',
+}
+
+export const lastRowBottomLeftStyle = {
+  position: 'absolute',
+  bottom: '-48px',
+  left: '-7px',
+  width: '10px',
+  height: '50px',
+  backgroundColor: 'cyan',
+  transform: 'rotate(-60deg)',
+}
+
+export const bottomRightStyle = {
+  position: 'absolute',
+  bottom: '-50px',
+  right: '61px',
+  width: '10px',
+  height: '50px',
+  backgroundColor: 'transparent',
+  border: '1px dashed limegreen',
+  transform: 'rotate(60deg)',
+}
+
+const roadPlacementStyleMap = {
+  topLeft: {
+    style: {
+      position: 'absolute',
+      top: '-24px',
+      left: '-2px',
+      width: '10px',
+      height: '50px',
+      backgroundColor: 'white',
+      transform: 'rotate(60deg)',
+    },
+  },
+  topRight: {
+    style: {
+      position: 'absolute',
+      top: '-23px',
+      right: '62px',
+      width: '10px',
+      height: '50px',
+      backgroundColor: 'purple',
+      transform: 'rotate(-60deg)',
+    },
+  },
+  left: {
+    style: {
+      position: 'absolute',
+      top: '39px',
+      left: '-38px',
+      width: '10px',
+      height: '50px',
+      backgroundColor: 'blue',
+      transform: 'rotate(0deg)',
+    },
+  },
+
+  botLeft: {
+    style: {
+      position: 'absolute',
+      bottom: '-48px',
+      left: '-7px',
+      width: '10px',
+      height: '50px',
+      backgroundColor: 'cyan',
+      transform: 'rotate(-60deg)',
+    },
+  },
+  botRight: {
+    style: {
+      position: 'absolute',
+      bottom: '-50px',
+      right: '61px',
+      width: '10px',
+      height: '50px',
+      backgroundColor: 'transparent',
+      border: '1px dashed limegreen',
+      transform: 'rotate(60deg)',
+    },
+  },
+  right: {
+    style: {
+      position: 'absolute',
+      top: '39px',
+      right: '25px',
+      width: '10px',
+      height: '50px',
+      backgroundColor: 'burlywood',
+      transform: 'rotate(0deg)',
+    },
+  },
+}
+
+function RoadPlacement({ placement }) {
+  return <Box sx={{ ...roadPlacementStyleMap[placement].style }} />
+}
+
+export default RoadPlacement
