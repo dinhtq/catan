02322dcd1d4209a1ea1145be27c06bfd0b5e7cc2
Jason Dinh <dinhtq1@gmail.com>
2024-10-12 01:24:14 -0700

    added blur to cur player

--- src/components/Players/Players.jsx
+++ src/components/Players/Players.jsx
@@ -1,4 +1,4 @@
-import { Paper, Box, Typography } from '@mui/material'
+import { Paper, Box, Typography, colors } from '@mui/material'
 import { useMemo } from 'react'
 import { COLORS, devCardsTypes, piecesTypes } from '../../utils/constants'
 
@@ -38,26 +38,61 @@ function Player({
     return totalPoints
   }, [player.devCards])
 
-  return (
-    <Box
-      key={player.playerId}
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        gap: '0.2rem',
-        padding: '10px 10px 10px 10px',
-        borderRadius: '10px',
-        border: isActivePlayer ? '1px solid green' : 'none',
-        backgroundColor: color,
-        color: color === COLORS.TEAMS.white ? 'black' : 'inherit',
-      }}
-    >
-      <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-        <Typography>Player {player.playerId}</Typography>
-        <Typography>Points {playerPoints}</Typography>
-      </Box>
+  const style = useMemo(() => {
+    let sty = {
+      display: 'flex',
+      flexDirection: 'column',
+      gap: '0.2rem',
+      //padding: '10px 10px 10px 10px',
+      borderRadius: '10px',
+      border: isActivePlayer ? '1px solid green' : 'none',
+      backgroundColor: color,
+      color: color === COLORS.TEAMS.white ? 'black' : 'inherit',
+      position: 'relative',
+    }
+    if (isActivePlayer) {
+      sty = {
+        ...sty,
+        '&:before': {
+          content: '""',
+          position: 'absolute',
+          top: '-2px',
+          left: '-2px',
+          right: '-2px',
+          bottom: '-2px',
+          //background: #fff;
+          //background: 'red',
+          zIndex: -1,
+          //border: '1px solid yellow',
+          background: 'linear-gradient(235deg, #89ff00, #010615, #00bcd4)',
+        },
+        '&:after': {
+          content: '""',
+          position: 'absolute',
+          top: '-2px',
+          left: '-2px',
+          right: '-2px',
+          bottom: '-2px',
+          //background: '#fff',
+          zIndex: -2,
+          filter: 'blur(13px)',
+          background: 'linear-gradient(235deg, #89ff00, #010615, #00bcd4)',
+        },
+      }
+    }
+    return sty
+  }, [isActivePlayer])
 
-      <Box sx={{}}>resources</Box>
+  return (
+    <Box key={player.playerId} sx={style}>
+      <Box sx={{ position: 'relative', zIndex: 2, backgroundColor: color }}>
+        <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Typography>Player {player.playerId}</Typography>
+          <Typography>Points {playerPoints}</Typography>
+        </Box>
+
+        <Box sx={{}}>resources</Box>
+      </Box>
     </Box>
   )
 }
